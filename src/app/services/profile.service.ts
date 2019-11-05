052fde1b1885174259cd74ccc674f3e163a05c9b
vique254 <victoronyango051@gmail.com>
2019-11-05 13:12:49 +0300

    create services and call back

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http,Headers} from '@angular/http';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 @Injectable({
   providedIn: 'root'
@@ -8,5 +8,11 @@ export class ProfileService {
 private username:string;
 private clientid ='39757cff39b6d5dabe5c';
 private clientsecret='28d3f70395eba21da000d5ed6cf681596fd275ab';
-  constructor(private http :Http) { }
+  constructor(private http :HttpClient) {
+    console.log("service is now ready")
+    this.username = 'vique254';
+   }
+   getProfileInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid +"&client_secret" + this.clientsecret );
+   }
 }
